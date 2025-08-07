5b0b912597d53c10530649b12ff8db8b479b588b
nehal123-dev <nehalnikhare256@gmail.com>
2025-08-07 09:33:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "aws_security_group" "jenkins-sg-2022" {
   key_name = var.key_name
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+   subnet_id = subnet-0e6e38d8aca8b4544
   tags= {
     Name = var.tag_name
   }
